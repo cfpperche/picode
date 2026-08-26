05b7a6115597b797dd2fe90cee0281f154a7f8a1
cfpperche <cfpperche@gmail.com>
2026-08-26 10:19:17 -0300

    revert: hanging-file tab shape; keep icon tabs
    quality-gate: PASS

--- internal/web/public/assets/swimlanesDiagram-VR7AAH4N-BxScMcgh.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-gQQP3WDS.js";import{_ as a}from"./mermaid.core-CGthN7wV.js";import"./chunk-5VM5RSS4-CbDLnva1.js";import"./chunk-XXDRQBXY-YVTPgbZI.js";import"./chunk-POPQ4Y6H-CTIJLn-A.js";import"./chunk-F27PBJKO-DVki8Qz4.js";import"./channel-C9vDsvub.js";import"./index-B5is1HIW.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
